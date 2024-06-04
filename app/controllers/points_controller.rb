c1aff1c6e078bd49acacc446ee67449eb3e3e8e8
sumika6211 <153085192+sumika6211@users.noreply.github.com>
2024-06-04 12:45:55 +0000

    Rubocop学習

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -13,27 +13,25 @@ class PointsController < ApplicationController
   def create
     @point = Point.new(point_params)
     if @point.save
-      flash[:notice] = 'ポイントを登録しました'
+      flash[:notice] = "ポイントを登録しました"
       redirect_to root_url
     else
       @points = Point.all
       @points_json = @points.map { |o| point_to_hash(o) }.to_json
-      flash.now[:alert] = 'ポイントを登録できませんでした'
+      flash.now[:alert] = "ポイントを登録できませんでした"
       render :index
     end
   end
 
   private
+    def point_params
+      params.require(:point).permit(:name, :latitude, :longitude, :address, :keyword)
+    end
 
-  def point_params
-    params.require(:point).permit(:name, :latitude, :longitude, :address, :keyword)
-  end
-
-  def point_to_hash(point)
-    { id: point.id,
-      name: point.name,
-      lat: point.latitude,
-      lng: point.longitude }
-  end
-
+    def point_to_hash(point)
+      { id: point.id,
+        name: point.name,
+        lat: point.latitude,
+        lng: point.longitude }
+    end
 end
